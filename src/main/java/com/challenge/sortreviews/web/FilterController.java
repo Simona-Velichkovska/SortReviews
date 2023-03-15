8693e1540f601e638ddfaadba8bb5a6831f6aebd
Simona Velichkovska <moni96girl@hotmail.com>
2023-03-15 18:36:23 +0100

    Changes to the filter page and initializing the controller for loading the page

--- src/main/java/com/challenge/sortreviews/web/FilterController.java
+++ src/main/java/com/challenge/sortreviews/web/FilterController.java
@@ -1,6 +1,20 @@
 package com.challenge.sortreviews.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+@RequestMapping("/")
 public class FilterController {
-
+    @GetMapping
+    public String getFilterPage(@RequestParam(required = false) String error, Model model){
+        if(error!=null && !error.isEmpty()){
+            model.addAttribute("hasError",true);
+            model.addAttribute("error",error);
+        }
+        return "filterpage";
+    }
 }
