b69a69972de48bf5bd78b5154a9ea2114fa930e3
Simona Velichkovska <moni96girl@hotmail.com>
2023-03-15 14:28:40 +0100

    Initial Commit

---
+++ src/main/java/com/challenge/sortreviews/SortReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.sortreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortReviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenge/sortreviews/SortReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.sortreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
